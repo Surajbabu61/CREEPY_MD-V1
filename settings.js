f377a1246ea8c7a36dbb7918b441801c6313088a
Surajbabu61 <155886339+Surajbabu61@users.noreply.github.com>
2024-12-28 19:24:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255697608274"
+global.ownernomer = "6282211756454"
 global.ownername = "𝒟𝒶𝓃𝓃𝓎"
-global.ownernumber = '255697608274'  //owner number 2
+global.ownernumber = '6282211756454'  //owner number 2
 //put your phone number above
 //let the owner as Danny or you can just put your name
 
